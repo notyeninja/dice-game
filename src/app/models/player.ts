4561760040a6ec9f09c893c1dca52def81b7d490
notyeninja <notyeninja@gmail.com>
2020-02-09 23:03:38 -0800

    added provision for player avatar

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -9,8 +9,11 @@ import uuid from 'uuid/v1';
 export class Player {
 
   id:string;
+  avatar:string;
   _currentScore:number;
   private _totalScore:number;
+  private _urlEndpoint:string  = "https://api.adorable.io/avatars/64/bot";
+  private _availableAlphabet = "abcdetsfghimn"
 
   get totalScore():number{
     return this._totalScore;
@@ -25,6 +28,12 @@ export class Player {
   constructor(public name){
     this._currentScore = 0;
     this._totalScore = 0;
+    this.avatar = this.getAvatarUrl();
     this.id = uuid();
+  }
+
+  private getAvatarUrl():string{
+     let char = this._availableAlphabet.charAt(Math.floor(Math.random() * this._availableAlphabet.length));
+     return `${this._urlEndpoint}${char}@adorable.png`;
   }
 }
